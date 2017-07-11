6b0c76cfef991446dbc19dc593858cf6cafac683
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-10 21:39:15 -0400

    i cant get the stop button to work

--- homework/unit_01/javaScript/stopwatch/hw.js
+++ homework/unit_01/javaScript/stopwatch/hw.js
@@ -18,47 +18,71 @@ const Stopwatch = {
   laps: [],
   // DO NOT EDIT ABOVE THIS LINE
   advanceTenMillisecs: function(){
-    // Your Code Here
+    this.millisecs += 10;
+    if(this.millisecs >= 1000) {
+      this.millisecs -= 1000;
+      this.secs++;
+    }
+    if(this.secs >= 60) {
+      this.sec -= 60;
+      this.mins++;
+    }
   },
   reset: function(){
-    // Your Code Here
+    this.millisecs = 0;
+    this.secs = 0;
+    this.mins = 0;
   },
   start: function(){
-    // Your Code Here
+    if(!this.isRunning) {
+      this.isRunning = true;
+      this.tickClock();
+    }
   },
-  stop: function(){
-    // Your Code Here
+  stop: function() {
+    this.running = false;
   },
   lap: function(){
-    // Your Code Here
+    
   }
 };
 
 /// User Interface ///
 const ViewEngine = {
   updateTimeDisplay: function(mins, secs, millisecs){
-    // Your Code Here
-  },
+  document.getElementById('mins').innerHTML = ViewHelpers.zeroFill(mins, 2);
+    document.getElementById('secs').innerHTML = ViewHelpers.zeroFill(secs, 2);
+    document.getElementById('millisecs').innerHTML = ViewHelpers.zeroFill(millisecs/10, 2);
+   },
   updateLapListDisplay: function(laps){
     // Your Code Here
   },
 };
 const ViewHelpers = {
   zeroFill: function(number, length){
-    // Your Code Here
+    var str = number.toString();
+    let numZeroes = Math.max(length - str.length, 0);
+    for( var i = 0; i < (length - str.length); i++){
+      str = '0' + str;
+    }
+    return str;
   },
 };
 
 /// Top-Level Application Code ///
 const AppController = {
   handleClockTick: function(){
-    // Your Code Here
+    ViewEngine.updateTimeDisplay(Stopwatch.mins, Stopwatch.secs, Stopwatch.millisecs);
   },
   handleClickStart: function() {
-    // Your Code Here
+    if (!Stopwatch.isRunning) { Stopwatch.start(); }
   },
   handleClickStopReset: function(){
-    // Your Code Here
+      if (Stopwatch.isRunning) {
+      Stopwatch.stop();
+    } else {
+      Stopwatch.reset();
+    }
   },
   handleClickLap: function(){
     // Your Code Here
@@ -67,4 +91,7 @@ const AppController = {
 
 window.onload = function(){
   // Attach AppController methods to the DOM as event handlers here.
+  document.getElementById('start').onclick = AppController.handleClickStart;
+  document.getElementById('stop').onclick = AppController.handleClickStopReset;
 };
+
