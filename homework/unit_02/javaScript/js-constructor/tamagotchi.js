6d667d15fffd122f88bccb224a35e4935c363544
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-24 19:07:50 -0400

    project 2

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,27 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
+class Tamagotchi {
+    constructor(name, creatureType) {
+        this.name = name;
+        this.creatureType = creatureType;
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+    }
+    cry() {
+        this.foodInTummy--;
+        console.log("Waaaaa");
+        console.log(this.name + "has this much food " + this.foodInTummy);
+    }
+}
+
 
 
 //create new Tamagotchis
-
+var newTamagotchi1 = new Tamagotchi();
+var newTamagotchi2 = new Tamagotchi();
 
 //test out your Tamagotchies below via console.logs
+console.log(newTamagotchi1);
+console.log(newTamagotchi2);
--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -4,19 +4,26 @@ const fs = require('fs');
 
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
+// fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+//   if (error) {
+//     console.error(error);
+//   }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
+//   // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
 
-  let lines = content.split('\n');
+//   let lines = content.split('\n');
 
-  // clean up the array by removing the empty line
-  lines.pop();
+//   // clean up the array by removing the empty line
+//   lines.pop();
 
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
-  });
+//   lines.forEach(function(line) {
+//     console.log('Hello, ' + line + '!');
+//   });
+// });
+
+const readFilePromise = new Promise((resolve, reject) => {
+    fs.readFile(inFile, { encoding: 'utf8' }, (error, content) => {
+        resolve(content);
+        reject('there is an error here ', error);
+    });
 });
