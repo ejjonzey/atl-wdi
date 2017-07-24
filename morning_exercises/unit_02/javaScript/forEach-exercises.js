df589adb0434af1b4f086014d77e0d978b9f8a25
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-24 15:08:32 -0400

    gometry spec

---
+++ morning_exercises/unit_02/javaScript/forEach-exercises.js
@@ -0,0 +1,6 @@
+words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+
+words.forEach(function(index, item) {
+    words[index] = item.toUpperCase();
+});
+console.log(item);
