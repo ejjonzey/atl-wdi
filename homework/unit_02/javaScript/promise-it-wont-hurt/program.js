ef3cb394d755180bf171a2cd189febc17da6f8a8
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-24 22:30:12 -0400

    reject or not reject

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -13,13 +13,24 @@
 // promise.then(console.log);
 
 //reject promise
-var promise = new Promise(function(fulfill, reject) {
-    setTimeout(function() {
-        reject(new Error("REJECTED!"))
-    }, 300);
-});
+// var promise = new Promise(function(fulfill, reject) {
+//     setTimeout(function() {
+//         reject(new Error("REJECTED!"))
+//     }, 300);
+// });
+
+// function onReject(error) {
+//     console.log(error.message);
+// }
+// promise.then(null, onReject);
+
+//reject or not reject
+var promise = new Promise((fulfill, reject) => {
+    fulfill('I FIRED');
+    reject(new Error('I DID NOT FIRE!'));
+})
 
 function onReject(error) {
     console.log(error.message);
 }
-promise.then(null, onReject);
+promise.then(console.log, onReject);
