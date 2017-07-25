05695549299700bfb74f789e6cee3c2e553580bc
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-24 21:54:39 -0400

    first two problems done

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -1,4 +1,12 @@
 //warm-up
-setTimeout(() => {
-    console.log('TIMED OUT!');
-}, 300);
+// setTimeout(() => {
+//     console.log('TIMED OUT!');
+// }, 300);
+
+var promise = new Promise(function(fulfill, reject) {
+    setTimeout(function() {
+        fulfill('FULFILLED!')
+    }, 300);
+});
+
+promise.then(console.log);
