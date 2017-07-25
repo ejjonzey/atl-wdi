c17ca4bc977e97e43ea14eb4ecade65130519312
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-24 21:59:53 -0400

    promise three

--- homework/unit_02/javaScript/promise-it-wont-hurt/program.js
+++ homework/unit_02/javaScript/promise-it-wont-hurt/program.js
@@ -3,10 +3,23 @@
 //     console.log('TIMED OUT!');
 // }, 300);
 
+//fulfill promise
+// var promise = new Promise(function(fulfill, reject) {
+//     setTimeout(function() {
+//         fulfill('FULFILLED!')
+//     }, 300);
+// });
+
+// promise.then(console.log);
+
+//reject promise
 var promise = new Promise(function(fulfill, reject) {
     setTimeout(function() {
-        fulfill('FULFILLED!')
+        reject(new Error("REJECTED!"))
     }, 300);
 });
 
-promise.then(console.log);
+function onReject(error) {
+    console.log(error.message);
+}
+promise.then(null, onReject);
