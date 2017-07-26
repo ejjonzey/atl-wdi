a85b123d8fb9a86c347197cd37c0d65c68496c5a
Ejjonzey <ejjonzey1982@gmail.com>
2017-07-26 11:20:20 -0400

    mongoose lesson starter

--- labs/unit_02/mongo/express-mongoose-lesson-starter/controllers/usersController.js
+++ labs/unit_02/mongo/express-mongoose-lesson-starter/controllers/usersController.js
@@ -5,14 +5,63 @@ var User = require("../models/user");
 var Item = require("../models/item");
 
 // USERS INDEX ROUTE
+router.get('/', (req, res) => {
+    //console.log('hi');
+    User.find({})
+        .then((users) => {
+            res.render('users/index', {
+                users: users
+            });
+        })
+        .catch((error) => {
+            console.log(`error!!! ${error}`)
+        });
+});
 
-// USER SHOW ROUTE
+router.get('/new', (req, res) => {
+    res.render('users/new');
+});
 
 // USER CREATE ROUTE
+router.post('/', (req, res) => {
+    //res.send(req.body);
+    const newUser = new User(req.body);
+    newUser.save()
+        .then((newUser) => {
+            res.render('users/new', {
+                user: newUser
+            })
+        })
+        .catch((err) => {
+            if (err) {
+                console.log('there is error here');
+            }
+        })
+})
+
+// USER SHOW ROUTE
+router.get('/:id', (req, res) => {
+    Users.findById(req.params.id)
+        .then((users) => {
+            res.render('users/show', {
+                user: user
+            })
+        })
+});
+
+
+
 
 // USER UPDATE ROUTE
 
 // USER DESTROY
+router.delete('/:id/delete', (req, res) => {
+    const userIdToDelete = req.params.id;
+    User.findByIdAndRemove(userIdToDelete)
+        .then(() => {
+            res.redirect('/users');
+        })
+})
 
 // ADD A NEW ITEM
 
