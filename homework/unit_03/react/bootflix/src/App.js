18da9957e393d63c6ee2af4cebd48539eda8fa52
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-08 08:56:58 -0400

    It works, finally

--- homework/unit_03/react/bootflix/src/App.js
+++ homework/unit_03/react/bootflix/src/App.js
@@ -9,33 +9,75 @@ import example from './omdbExample.json';
 class App extends Component {
   constructor(){
     super();
-    this.state = {
-      movie: example
+
+        this.state = {
+      title: "",
+      year: 0,
+      director: "",
+      plot: "",
+      genre: "",
+      poster: "",
     }
   }
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
-  _searchByTitle = () => {
-    axios.get('http://img.omdbapi.com/?i=tt2294629&apikey=d31f1a94')
-    .then((res)=>{
-      console.log(res);
-      const url = res.data.Title;
-      this.setState({Title: url});
-    })
-    console.log("Search by Title");
+_searchByTitle = (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    axios.get(`http://www.omdbapi.com/?t=${title}&apikey=d31f1a94`)
+      .then((res) => {
+        this.setState({
+          title: res.data.Title,
+          year: res.data.Year,
+          director: res.data.Director,
+          plot: res.data.Plot,
+          genre: res.data.Genre,
+          poster: res.data.imdbID,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
   }
 
-  _searchById = () => {
-    console.log("Search by ID");
+   _searchById = (event) => {
+    event.preventDefault();
+    const id = event.target.id.value;
+    axios.get(`http://www.omdbapi.com/?i=${id}&apikey=d31f1a94`)
+      .then((res) => {
+        this.setState({
+          title: res.data.Title,
+          year: res.data.Year,
+          director: res.data.Director,
+          plot: res.data.Plot,
+          genre: res.data.Genre,
+          poster: res.data.imdbID
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
-  render() {
+   render() {
+
     return (
       <div className="App">
         <Header />
-        <Search />
-        <Movie />
+        <Search 
+          searchByTitle = {this._searchByTitle}
+          searchById = {this._searchById}
+          />
+        <Movie 
+            title={this.state.title || "Search for a movie"}
+            year={this.state.year || "search"}
+            director={this.state.director || "find a movie"}
+            plot={this.state.plot || "Search for a movie using the search bar above."}
+            genre={this.state.genre || "exploration"}
+            poster={this.state.poster}
+            />
       </div>
     );
   }
--- homework/unit_03/react/bootflix/src/components/Movie.jsx
+++ homework/unit_03/react/bootflix/src/components/Movie.jsx
@@ -1,18 +1,23 @@
+
 import React from "react";
 
 // Update this Movie component with info from OMDB
 // BONUS: Use OMDB's Poster API to add a poster to each movie.
-const Movie = () => {
+const Movie = (props) => {
+  const poster = `http://img.omdbapi.com/?i=${props.poster}&apikey=d31f1a94`
   return (
     <section id="movie-listing">
       <div className="movie">
-        <h3>Movie Title</h3>
+        <h3>{props.title}</h3>
         <p>
-          <strong>Released:</strong> 1984<br />
-          <strong>Directed By:</strong> Some director<br />
-          <em>Genre:</em>
+          <strong>Released:</strong> {props.year}<br />
+          <strong>Directed By:</strong> {props.director}<br />
+          <em>Genre: {props.genre}</em>
         </p>
-        <p>Plotline should go inside of here</p>
+        <p>{props.plot}</p>
+      </div>
+      <div className="poster-container">
+      <img src={poster} alt=""></img>
       </div>
     </section>
   );
