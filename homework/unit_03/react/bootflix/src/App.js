db642aabee35744dab99824690a4b894cf7b486d
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-07 21:42:30 -0400

    bootflix

--- homework/unit_03/react/bootflix/src/App.js
+++ homework/unit_03/react/bootflix/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
-import example from './omdbExample.json'
+import example from './omdbExample.json';
+
 
 class App extends Component {
   constructor(){
@@ -14,6 +16,12 @@ class App extends Component {
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
   _searchByTitle = () => {
+    axios.get('http://img.omdbapi.com/?i=tt2294629&apikey=d31f1a94')
+    .then((res)=>{
+      console.log(res);
+      const url = res.data.Title;
+      this.setState({Title: url});
+    })
     console.log("Search by Title");
   }
 
