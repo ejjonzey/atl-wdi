fe13f75dcabdac3fb01ffae9eafefaf26dd33cba
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-17 19:51:39 -0400

    found carmen.

---
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -0,0 +1,13 @@
+Clue #1: SELECT * FROM country WHERE region = 'Southern Europe' ORDER BY population DESC;
+
+Clue #2: SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
+
+Clue #3: SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = '100';
+
+Clue #4: SELECT * FROM city WHERE countrycode = 'SMR';
+
+Clue #5: SELECT * FROM city WHERE name LIKE 'Ser%';
+
+Clue #6: SELECT * FROM country WHERE code = 'BRA';
+
+Shes in: SELECT * FROM city WHERE population = 91084;
