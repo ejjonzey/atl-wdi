589facc75a42e24211041d393aeb71c2e1b97e80
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-18 08:50:06 -0400

    finished nfl queries.

--- homework/sql/nfl/nfl-seed-data/nfl.sql
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -13,3 +13,15 @@ Query #6: SELECT * FROM players ORDER BY salary DESC LIMIT 50;
 Query #7: SELECT AVG(salary) FROM players;
 
 Query #8: SELECT name, position FROM players WHERE salary > 10000000;
+
+Query #9: SELECT * FROM players ORDER BY salary DESC LIMIT 1;
+
+Query #10: SELECT name, position FROM players ORDER BY salary ASC LIMIT 100;
+
+Query #11: SELECT AVG(salary) FROM players WHERE position = 'DE';
+
+Query #12: SELECT * FROM players WHERE team_id = '1';
+
+Query #13: SELECT SUM(salary) FROM players WHERE team_id = '18';
+
+Query #14: SELECT name FROM players WHERE team_id = '23' ORDER BY salary ASC LIMIT 1;
