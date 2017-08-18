7467dadf7936633d82ca0d87c6c12f7d76bf3370
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-18 08:47:01 -0400

    lots of nfl data.

--- homework/sql/nfl/nfl-seed-data/nfl.sql
+++ homework/sql/nfl/nfl-seed-data/nfl.sql
@@ -6,4 +6,10 @@ Query #3: SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = '
 
 Query #4: SELECT COUNT(*) FROM players;
 
-Query #5: 
+Query #5: SELECT name, head_coach FROM teams WHERE conference = 'AFC' OR conference = 'NFC' AND division = 'North' OR division = 'East';
+
+Query #6: SELECT * FROM players ORDER BY salary DESC LIMIT 50;
+
+Query #7: SELECT AVG(salary) FROM players;
+
+Query #8: SELECT name, position FROM players WHERE salary > 10000000;
