4a37a8a661d6a7285a0a65ff39ed2600cc60d964
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-21 19:15:49 -0400

    String skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -12,9 +12,18 @@
 #print test
 
 # Integers:
-d = 13
-puts d
-e = 7.3
-puts e
-d = e
-puts d
+# d = 13
+# puts d
+# e = 7.3
+# puts e
+# d = e
+# puts d
+
+# Strings:
+# christine = "Hi, I'm Christine"
+# likesTo = "long walks on the beach, kinda."
+# puts "#{christine} and I like #{likesTo}"
+# g = "2"
+# gToNumber = 2
+# puts g.to_i+ gToNumber
+
