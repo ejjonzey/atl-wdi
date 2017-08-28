0cfcf88ed19d9af6526bb294f90e7b8d1b17edbe
Ejjonzey <ejjonzey1982@gmail.com>
2017-08-28 09:05:26 -0400

    tip-calculator.

--- morning_exercises/tip-calculator/main.rb
+++ morning_exercises/tip-calculator/main.rb
@@ -1,59 +1,50 @@
 # asking for subtotal
-p "whats your subtotal"
-subtotal = gets.chomp.to_f
+puts "What is your subtotal?"
+subtotal = gets.chomp
 
-p subtotal
+puts subtotal
 
-p "number of people"
-people = gets.chomp.to_f
+puts "How many people in your party?"
+people = gets.chomp
 
-p people
+puts people
 
-puts %q{
-  how much tip to leave?
-  1. 15%
-  2. 18%
-  3. 20%
-  4. other
-}
+puts "How much of a tip would you like to leave?"
+puts "A = 15%"
+puts "B = 18%"
+puts "C = 20%"
+puts "D = other"
+option = STDIN.gets.chomp.upcase
 
-option = STDIN.gets.chomp
-p option
-
-case option
-  when "1"
-    tip_percent = 0.15
-  when "2"
-    tip_percent = 0.18
-  when "3"
-    tip_percent = 0.20
-  else
-    puts "Invalid option, assuming 50% tip"
-    tip_percent = 0.50
+if option.upcase == "A"
+  tip_percent = 0.15
+elsif option.upcase == "B"
+  tip_percent = 0.18
+elsif option.upcase == "C"
+  tip_percent = 0.20
+elsif option.upcase == "D"
+  puts "Let me choose?  That'll be 50%!"
+  tip_percent = 0.50
 end
+puts tip_percent
 
-p tip_percent
-
-p "What is the tax percentage? (eg 5, 6, 7)"
+puts "What is your tax percentage?"
 tax_percent = gets.chomp.to_f
-
-p tax_percent
-
-
+puts tax_percent
 
 tax_value = subtotal * (tax_percent/100.0)
-# meal_with_tax = subtotal + tax_value
-p 'tax value is #{tax_value}'
-p tax_value
+
+puts "Tax value is #{tax_value}"
+puts tax_value
+
 tip_value = subtotal * tip_percent
-p tip_value
+total = subtotal + tax_value + tip_value
+total_per_person = total/people
+
+puts "total is  #{total}"
+puts "Each person owes #{total_per_person}"
+
+# meal_with_tax = subtotal + tax_value
+
 # total_value = meal_with_tax * tip_percent/100
 # total = meal_with_tax + tip_value
-total = subtotal + tax_value + tip_value
-total_pp = total/people
-
-puts "total is #{total}"
-puts "each person pays #{total_pp}"
-
-
-
